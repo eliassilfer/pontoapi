7c3d481e329469d2ef127bade4b0060c9ef5c7d3
Elias <eliassilfer@gmail.com>
2018-05-20 15:30:36 -0300

    primeito commit

---
+++ src/main/java/com/saile/pontoapi/PontoapiApplication.java
@@ -0,0 +1,12 @@
+package com.saile.pontoapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PontoapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PontoapiApplication.class, args);
+	}
+}
